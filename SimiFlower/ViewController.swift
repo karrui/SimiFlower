cc81aa94a0a1d802a900f16fd7dc8ed9518cb6d8
Karrui Lau <e0032073@u.nus.edu>
2018-01-05 22:12:16 +0800

    Add CoreML detect method to detect what flower it is

--- SimiFlower/ViewController.swift
+++ SimiFlower/ViewController.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    var pickedImage: UIImage?
     
     let imagePicker = UIImagePickerController()
     
@@ -34,9 +38,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             guard let ciImage = CIImage(image: userPickedImage) else {
                 fatalError("Could not convert UIImage to CIImage")
             }
+            pickedImage = userPickedImage
+            
+            detect(flowerImage: ciImage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(flowerImage: CIImage) {
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            fatalError("Failed to initialize model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Failed to obtain results")
+            }
+            
+            if let firstResult = results.first {
+                let identifier = firstResult.identifier
+                self.navigationItem.title = identifier
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: flowerImage)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
