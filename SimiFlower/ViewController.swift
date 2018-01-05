b0dd690c2d89ad2e11f031b39df62b5b6d80a770
Karrui Lau <e0032073@u.nus.edu>
2018-01-05 21:44:45 +0800

    Initial Commit

---
+++ SimiFlower/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimiFlower
+//
+//  Created by Karrui Lau on 5/1/18.
+//  Copyright © 2018 Karrui Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
