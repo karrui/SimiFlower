018cbadf4463574692f9b60be84dfdb99f68227d
Karrui Lau <e0032073@u.nus.edu>
2018-01-05 21:58:38 +0800

    Initial UI design and linking of camera to application

--- SimiFlower/ViewController.swift
+++ SimiFlower/ViewController.swift
@@ -8,11 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .camera
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let userPickedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            imageView.image = userPickedImage
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage to CIImage")
+            }
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
